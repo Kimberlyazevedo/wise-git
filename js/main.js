cc61d4ea4ad1b47e72e10fffb89398008d4d425f
Kimberly Azevedo <kimyazevedo@gmail.com>
2022-09-20 10:28:04 -0300

    JS em desenvolvimento

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
 var icones = document.querySelectorAll(".menu__principal-icone");
 
-
 for (var i = 0; i < icones.length; i++) {
     icones[i].addEventListener("click", function() {
         document.querySelector(".active")
             .classList.remove("active")
-        this.classList.add("active")
-
-        // alert(this.getAttribute("alt"));
-
-    })
-}
-
-function toggleTabs(tab) {
-    const collection = document.getElementsByClassName("activeTab");
-    for (var i = 0; i < collection.length; i++) {
-        collection[i].classList.remove("activeTab");
-    }
-    let tabAtiva = document.getElementById(tab)
-    tabAtiva.classList.add("activeTab")
+        this.classList.add("active");
+    });
 }
