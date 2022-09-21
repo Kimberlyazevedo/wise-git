f062e6841676c1a2548f9159440c9605a62a12cc
Kimberly Azevedo <kimyazevedo@gmail.com>
2022-09-20 23:13:06 -0300

    Página finalizada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 var icones = document.querySelectorAll(".menu__principal-icone");
 
+
 for (var i = 0; i < icones.length; i++) {
-    icones[i].addEventListener("click", function() {
-        document.querySelector(".active")
-            .classList.remove("active")
+
+    icones[i].addEventListener("click", function(event) {
+        document.querySelector(".active").classList.remove("active")
         this.classList.add("active");
-    });
+
+        console.log(event);
+        var pagina = this.getAttribute("data-page")
+
+
+        document.querySelector("section.active").classList.remove("active")
+        document.getElementById(pagina).classList.add("active")
+
+
+    })
 }
